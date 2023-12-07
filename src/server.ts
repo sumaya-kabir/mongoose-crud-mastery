d8187a2a95d6836f58af88a531b26e1a91fa6059
sumaya-kabir <sumayakabir4547@gmail.com>
2023-12-07 19:34:37 +0600

    problems occured

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import app from './app';
 import mongoose from 'mongoose';
 import config from './app/config';
 
-async function main() {
+function main() {
   try {
-    await mongoose.connect(config.database_url as string);
+    mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
       console.log(`app is listening on port ${config.port}`);
